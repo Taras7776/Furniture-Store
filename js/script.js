47d8998dbfc96b7268a68406003ed9432cbbd2ff
Taras Lozovyi <lozoviitaras777@email.com>
2026-06-27 11:12:21 +0300

    replaced js structure

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ const burgerButton = document.querySelector(".header__burger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 burgerButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("is-open");
-
     burgerButton.classList.toggle("is-open");
+    // toggle - метод для перемикання станів, якщо CSS-клас існує на елементі — метод видаляє його; якщо відсутній — додає
+
+    mobileMenu.classList.toggle("is-open");
 
     document.body.classList.toggle("no-scroll");
 });
